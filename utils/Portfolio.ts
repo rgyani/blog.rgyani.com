7cb375f53c6829b66dc1f8ab14317189ebba6e14
Ravi Gyani <ravi.gyani@dxc.com>
2023-11-12 09:33:06 +0100

    merge commits

---
+++ utils/Portfolio.ts
@@ -0,0 +1,67 @@
+export type Portfolio = {
+  title: string;
+  subtitle: string;
+  thumb: string;
+  image: string;
+};
+
+
+export const PORTFOLIOS: Portfolio[] = [
+  {
+    title: "LetsBat 3D", subtitle: "Unity 3D Batting Simulation Game", thumb: "/portfolio/letsbat3d-thumb.png", image: ""
+  },
+  {
+    title: "LetsBat", subtitle: "A Cocos 2D batting simulation game for iOS devices", thumb: "/portfolio/letsbat-thumb.png", image: "/portfolio/letsbat.png"
+  },
+  {
+    title: "LetsRun", subtitle: "A Cocos 2D Side Scrolling game for iOS devices", thumb: "/portfolio/letsrun-thumb.png", image: "/portfolio/letsrun.png"
+  },
+  {
+    image: "/portfolio/letspark.png", subtitle: "A Cocos 2D Car Parking game for iOS devices", thumb: "/portfolio/letspark-thumb.png", title: "LetsPark"
+  },
+  {
+    image: "/portfolio/andro-settings.png", subtitle: "With Andro Settings you can turn ON/OFF various system settings like Wifi, Data, AutoRotate etc", thumb: "/portfolio/andro-settings-thumb.png", title: "AndroSettings"
+  },
+  {
+    image: "/portfolio/andro-smsfilter.png", subtitle: "With Andro SMSFilter, you can sort out incoming messages as Junk or otherwise, based on filters you have setup.", thumb: "/portfolio/andro-smsfilter-thumb.png", title: "Andro SMSFilter"
+  },
+  {
+    image: "/portfolio/andro-notes.png", subtitle: "Andro Notes enables you to create simple notes and password protect them", thumb: "/portfolio/andro-notes-thumb.png", title: "Andro Notes"
+  },
+  {
+    image: "/portfolio/knocks-grid.png", subtitle: "KNOCKS WinGrid, an advanced and highly extensible WinForms grid control for your .NET applications.", thumb: "/portfolio/knocks-grid-thumb.png", title: "GridView Controls"
+  },
+  {
+    image: "/portfolio/knocks-calendars.gif", subtitle: "KNOCKS Calendars give you outlook style calendars for your .NET applications", thumb: "/portfolio/knocks-calendars-thumb.png", title: "Calendar Controls"
+  },
+  {
+    image: "/portfolio/knocks-docking.gif", subtitle: "KNOCKS Docking Manager allows VS.NET or Office styled docking windows control for your WinForms applications.", thumb: "/portfolio/knocks-docking-thumb.png", title: "Docking Controls"
+  },
+  {
+    image: "/portfolio/knocks-ribbon.png", subtitle: "KNOCKS Ribbon Extender allows Office 2007 styled Ribbon look and feel for your WinForms applications.", thumb: "/portfolio/knocks-ribbon-thumb.png", title: "Ribbon Controls"
+  },
+  {
+    image: "/portfolio/knocks-instru.jpg", subtitle: "The KNOCKS Instrumentation library is a set of components built to display meters, guages, dials, switches for a WinForms HMI application.", thumb: "/portfolio/knocks-instru-thumb.jpg", title: "Instrumentation Suite"
+  },
+  {
+    image: "/portfolio/knocks-shell.jpg", subtitle: "KNOCKS WinShell Suite mimics all standard Windows Explorer features like Thumbnail, Group and Details/Report View, icons, context menus, drag drop, renaming, info tips and many others.", thumb: "/portfolio/knocks-shell-thumb.jpg", title: "Shell Controls"
+  },
+  {
+    image: "/portfolio/knocks-skinner.gif", subtitle: "KNOCKS Skinner helps build skinnable WinForms application, like the ever popular WinAmp UI.", thumb: "/portfolio/knocks-skinner-thumb.png", title: "Skinner Extender"
+  },
+  {
+    image: "/portfolio/wintweakz-disksuite.jpg", subtitle: " DiskSuite provides functionalities to manage user-level and system-level disk operations, like clean-up and optimization, data and free space defragmentation, and disk repair", thumb: "/portfolio/wintweakz-disksuite-thumb.jpg", title: "DiskSuite"
+  },
+  {
+    image: "/portfolio/wintweakz-passwordsafe.jpg", subtitle: "Store all your passwords and access codes on one digital key ring, protected with a single non-breakable password.", thumb: "/portfolio/wintweakz-passwordsafe-thumb.jpg", title: "PasswordSafe"
+  },
+  {
+    image: "/portfolio/wintweakz-privacyguard.jpg", subtitle: "PrivacyGuard guarantees your privacy by ensuring all traces of your online Internet and computer activities are permanently erased and unrecoverable on your PC.", thumb: "/portfolio/wintweakz-privacyguard-thumb.jpg", title: "Privacy Guard"
+  },
+  {
+    image: "/portfolio/wintweakz-regcleaner.jpg", subtitle: " With RegCleaner you can safely clean, repair and optimize the Windows® registry with a few simple mouse clicks!", thumb: "/portfolio/wintweakz-regcleaner-thumb.jpg", title: "Registry Cleaner"
+  },
+  {
+    image: "/portfolio/wintweakz-vault.jpg", subtitle: "WinTweakz Vault is your personal Virtual Safe to store your private documents, images, videos and files to protect them from prying eyes and data theft.", thumb: "/portfolio/wintweakz-vault-thumb.jpg", title: "Vault"
+  },
+];
