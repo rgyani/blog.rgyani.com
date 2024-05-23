9bcbda964510325841b9ddd58370614613d8e473
GitHub Action <action@github.com>
2024-05-23 09:31:07 +0000

    deploy

---
+++ _next/static/NR_QIFqf7T3lK6TvH8RkQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/478-142c2337897bd923.js","static/chunks/pages/index-bcd7888475b4858b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
